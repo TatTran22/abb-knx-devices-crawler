ed3c328fad855e9944ec435f03e5cdb049f899fa
tattran22 <tattran22@outlook.com>
2020-08-17 23:52:13 +0700

    craled 600 products

--- abb.js
+++ abb.js
@@ -18,8 +18,8 @@ const rp = require('request-promise');
 // };
 let knx = require('./ABB-KNX-Products-ID.json');
 
-const delayWriteFile = 30000; //30 second
-const delayLoad = 7000;
+const delayWriteFile = 60000; //60 second
+const delayLoad = 10000;
 let products_init = [];
 let products = [];
 // fs.readFile('./ABB-KNX-Products-ID.json', (err, data) => {
@@ -30,32 +30,53 @@ let products = [];
 // });
 
 // console.log('This is after the read call');
-let init = 0;
+let init = 500;
+let t0 = process.hrtime();
 craw(init);
 
 function craw(idx) {
-  setTimeout(function () {
-    console.log(idx);
-    crawlData(knx[idx], idx);
-    idx++;
-    if (idx < knx.length) {
-      if (idx == knx.length / 10) {
-        let productsFileName = `./crawled_data/ABB-KNX-Products${idx}.json`;
-        // console.log(products);
+  setTimeout(
+    function () {
+      console.log(idx);
+      crawlData(knx[idx], idx);
+      idx++;
+      let productsFileName = `./crawled_data/ABB-KNX-Products_${idx - 100}-${idx}.json`;
+      if (idx < knx.length) {
+        if (idx % 100 == 0 || idx == knx.length) {
+          // console.log(products);
 
-        let data = JSON.stringify(products, null, 2);
+          let data = JSON.stringify(products, null, 2);
 
-        setTimeout(() => {
-          fs.writeFile(productsFileName, data, (err) => {
-            if (err) throw err;
-            console.log(`Numer of products: ${products.length}`);
-            console.log(`Data written to file: ${productsFileName}`);
-          });
-          craw(idx);
-        }, 30000);
-      } else craw(idx);
-    }
-  }, delayLoad);
+          setTimeout(
+            () => {
+              fs.writeFile(productsFileName, data, (err) => {
+                if (err) throw err;
+                // console.log(`Numer of products: ${products.length}`);
+                console.log(`Data written to file: ${productsFileName}`);
+              });
+              let t1 = process.hrtime(t0);
+              console.log(`Crawled ${products.length} products in ${t1[1] / 1000000} milliseconds.`);
+              products = [];
+            },
+            delayWriteFile ? delayWriteFile : 30000
+          );
+        } else craw(idx);
+      } else {
+        // let data = JSON.stringify(products, null, 2);
+        // setTimeout(
+        //   () => {
+        //     fs.writeFile(productsFileName, data, (err) => {
+        //       if (err) throw err;
+        //       console.log(`Numer of products: ${products.length}`);
+        //       console.log(`Data written to file: ${productsFileName}`);
+        //     });
+        //   },
+        //   delayWriteFile ? delayWriteFile : 30000
+        // );
+      }
+    },
+    idx % 100 == 0 ? 0 : delayLoad
+  );
 }
 
 const crawlData = (id, index) => {
@@ -72,7 +93,15 @@ const crawlData = (id, index) => {
       const NLH = $("dd[data-code='ProductNetHeight']").text().split(' ');
       const NLW = $("dd[data-code='ProductNetWeight']").text().split(' ');
       const MOQ = $("dd[data-code='MinimumOrderQuantity']").text().split(' ');
-
+      let image = [];
+      let documentLink = [];
+      for (let i = 0; i < $("meta[itemprop='image']").length; i++) {
+        image.push($("meta[itemprop='image']")[i].attribs.content);
+      }
+      // console.log($('.document-link')[0].);
+      // for (let i = 0; i < $('.document-link').length; i++) {
+      //   console.log($('.document-link')[i].baseURI);
+      // }
       productInformation = {
         id: id,
         title: $('title').text().trim(),
@@ -81,17 +110,16 @@ const crawlData = (id, index) => {
         ean: $("dd[data-code='Ean']").first().text(),
         catalogDescription: $("dd[data-code='CatalogDescription']").text(),
         longDescription: $("dd[data-code='LongDescription']").text(),
-        imageUrl: [$("meta[itemprop='image']").first().attr('content')],
+        imageUrl: image,
         netLength: $("dd[data-code='ProductNetDepth'] span").text(),
         netHeight: $("dd[data-code='ProductNetHeight'] span").text(),
         netWidth: $("dd[data-code='ProductNetWidth'] span").text(),
         netWeight: $("dd[data-code='ProductNetWeight'] span").text(),
-        netLengthUnit: NLU[NLU.length - 1]
-          ? NLU[NLU.length - 1]
-          : NLH[NLH.length - 1],
+        netLengthUnit: NLU[NLU.length - 1] ? NLU[NLU.length - 1] : NLH[NLH.length - 1],
         netWeightUnit: NLW[NLW.length - 1],
         minimumOrderQuantity: MOQ[0],
         unit: MOQ[MOQ.length - 1],
+        documentLink: documentLink,
         originalLink: `https://new.abb.com/products/${id}/`,
         categories: $('.categories-section-wrapper li').text(),
       };
